8ec3864fd5f9ba9d562249e03e5ad0dc583bfd3e
Manoj Gupta <manoj334@gmail.com>
2021-05-13 13:10:47 +0530

    Initial Commit

---
+++ src/components/Cowin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Cowin extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Cowin Slot Checker</h1>
+            </div>
+        )
+    }
+}
+
+export default Cowin
